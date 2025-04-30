d30241074d2b436442ad0d3f73a31c934595d925
manish <manish.workholics@gmail.com>
2025-04-30 16:06:47 +0530

    update

--- backend/router/index.js
+++ backend/router/index.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 router.use('/admin', require('./admin.routes'));
 router.use('/users', require('./user.routes'));
-
+router.use('/auth', require('./auth.routes'));
 
 
 
